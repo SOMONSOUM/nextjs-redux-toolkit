7848d4cb5b20223b44a9e7c1578583c0a71eb9b7
SOMONSOUM <somon.soum9@gmail.com>
2024-02-12 10:56:17 +0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { fetchUsers } from "@/store/actions/usersAction";
+import { useAppSelector, useAppDispatch } from "../hooks/selector";
+import { RootState } from "@/store/store";
+export default function Home() {
+  const { users } = useAppSelector((state: RootState) => state.users);
+  const dispatch = useAppDispatch();
+  const handleClick = async () => {
+    dispatch(fetchUsers());
+  };
+
+  console.log("users", users);
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <button
+        className="bg-violet-500 text-center rounded-md text-slate-50 px-3 py-2"
+        onClick={handleClick}
+      >
+        Load Data
+      </button>
+
+      {}
+    </div>
+  );
+}
---
+++ app/user/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useAppDispatch, useAppSelector } from "@/hooks/selector";
+import { fetchUsers } from "@/store/actions/usersAction";
+import { RootState } from "@/store/store";
+
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  const { users } = useAppSelector((state: RootState) => state.users);
+  const dispatch = useAppDispatch();
+  const handleClick = () => {
+    dispatch(fetchUsers());
+  };
+
+  console.log("user", users);
+  return (
+    <div>
+      <button
+        onClick={handleClick}
+        className="bg-green-400 px-4 py-2 rounded-md shadow-sm"
+      >
+        Click Me
+      </button>
+      <div>
+        {users &&
+          users.map((user: any, i: string) => <div key={i}>{user.name}</div>)}
+      </div>
+    </div>
+  );
+};
+
+export default page;
